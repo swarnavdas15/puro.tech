26d91e615a72df600d397b46b343cbd562f3e37b
Swarnav Das <swarnavofcl15@gmail.com>
2026-01-24 21:59:52 +0530

    Day-1-a

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from '../components/Navbar.jsx'
-// import Hero from '../components/Hero.jsx'
+import Hero from '../components/Hero.jsx'
 
 function Home() {
   return (
